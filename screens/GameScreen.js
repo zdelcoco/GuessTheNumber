5af2705c4729942d154a0f8aeea4a1ec9df47c4b
Zachary Delcoco <zdelcoco@dmsi.com>
2024-05-20 13:09:01 -0500

    chore: Update Expo font plugin and dependencies

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -78,12 +78,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              <Ionicons name="md-remove" size={24} color="white" />
+              <Ionicons name="remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
-              <Ionicons name="md-add" size={24} color="white" />
+              <Ionicons name="add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
